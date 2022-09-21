cc1f6dccd634af5a548de80b2736f45167a6adc8
azaliazakirova <azalia-90@ya.ru>
2022-09-21 15:14:51 +0300

    home work

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -15,7 +17,7 @@ public class PracticeFormTest {
     static void setUp()
     {
         Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
     }
 
@@ -25,6 +27,8 @@ public class PracticeFormTest {
         String lastName = "Ivanov";
         String email = "ivan@test.ru";
         String mobile = "3246758909";
+        String subject = "English";
+        String address = "test street";
         open("/automation-practice-form");
         $("#firstName").setValue(name);
         $("#lastName").setValue(lastName);
@@ -32,6 +36,16 @@ public class PracticeFormTest {
         $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue(mobile);
         $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $("[class*='day--011']").click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("src/main/resources/1.png"));
+        $("#currentAddress").setValue(address);
+        $("#state .css-19bqh2r").click();
+        $(byText("NCR")).click();
+        $("#city .css-19bqh2r").click();
+        $(byText("Delhi")).click();
         $("#submit").click();
         $("#output #name").shouldHave(text(name));
     }
